f27e3d1c598f0ca4797e886e44bb318b67cb7f93
beebikun <beebikun@gmail.com>
2014-08-30 22:32:28 +1000

    сделано нормальное окошка выбора области обрезки

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-var CropImage = function(canvas){
+var CropImage = function(body){
     this.settings = new Object;
-    this.settings.rect = {
-        w: 200,
-        h: 200,
-        lineWidth: 5,
-        strokeStyle: 'black'
+    this.settings.croper = {
+        width: 200 + 'px',
+        height: 200 + 'px',
+        border: '4px solid black',
     }
-    this._init(canvas)
+    this._init(body)
 }
 
-CropImage.prototype._init = function(canvas) {
+CropImage.prototype._init = function(body) {
     this.dragok = false;
-    this.canvas = canvas;
-    this.context = canvas.getContext('2d');
-    this.w = canvas.getAttribute('width');
-    this.h = canvas.getAttribute('height');
-    this.rect = {x: 0, y: 0}
+    this.body = body;
+    this._createCanvas()
+    this.rectParams = {x: 0, y: 0}
     this.img = new Object
 
 
@@ -24,7 +21,6 @@ CropImage.prototype._init = function(canvas) {
     function _canvasMove(e){
         if ( self.dragok && self._drawRectCanMove(e) ){
             self._setRectPos(e)
-            self._redraw()
         }
     }
 
@@ -38,34 +34,53 @@ CropImage.prototype._init = function(canvas) {
         self.dragok = false;
     };
 
-    canvas.onmousedown = _canvasDown;
-    canvas.onmouseup = _canvasUp;
-    self.canvas.onmousemove = _canvasMove;
+
+
+
+/*    body.onmousedown = _canvasDown;
+    body.onmouseup = _canvasUp;
+    body.onmousemove = _canvasMove;*/
+    body.onmouseup = function(ev){
+        self.dragok = false
+    }
+    body.onmousemove = function(ev){
+        if(self.dragok){
+            var newX = ev.clientX - self.body.offsetLeft;
+            var newY = ev.clientY - self.body.offsetTop;
+            if(newX > 0 &&
+               newX < (self.w - self.croper.offsetWidth) &&
+               newY > 0 &&
+               newY < (self.h - self.croper.offsetHeight)){
+                self.croper.style.left = newX + 'px';
+                self.croper.style.top = newY + 'px';
+            }
+        }
+    }
 };
 
 
-CropImage.prototype._setRectPos = function(e) {
-    this.rect.x = e.pageX - this.canvas.offsetLeft;
-    this.rect.y = e.pageY - this.canvas.offsetTop;
+CropImage.prototype._createCanvas = function() {
+    this.canvas = canvas = document.createElement('canvas')
+    canvas.style.border = "1px solid red";
+    this.body.appendChild(canvas);
+    this.context = canvas.getContext('2d');
+    this.w = canvas.getAttribute('width');
+    this.h = canvas.getAttribute('height');
 };
 
-
-CropImage.prototype._drawRectCanMove = function(e) {
-    var left = this.canvas.offsetLeft;
-    var right = left + this.w - this.rect.w;
-    var top = this.canvas.offsetTop;
-    var bottom = top + this.h - this.rect.h;
-
-    var eventLeft = e.pageX;
-    var eventTop = e.pageY;
-
-    var moreThanLeftBorder = eventLeft > left;
-    var lessThanRightBorder = eventLeft < right;
-    var moreThanTopBorder = eventTop > top;
-    var lessThanBottomBorder = eventTop < bottom;
-    return moreThanLeftBorder && lessThanRightBorder &&
-           moreThanTopBorder && lessThanBottomBorder
+CropImage.prototype._createCroper = function() {
+    var self = this;
+    this.croper = croper = document.createElement('div')
+    croper.onmousedown = function(ev){
+        self.dragok = true
+    }
+    croper.onmouseup = function(ev){
+        self.dragok = false
+    }
+    croper.className = 'croper'
+    this.body.appendChild(croper)
 };
+
 
 CropImage.prototype._updateCanvasSize = function(w, h) {
     var c = this.canvas;
@@ -79,88 +94,52 @@ CropImage.prototype._updateCanvasSize = function(w, h) {
     }
 };
 
-CropImage.prototype._drawRect = function (w, h) {
-    var w = w || this.settings.rect.w;
-    var h = h || this.settings.rect.h;
-    var x = this.rect.x;;
-    var y = this.rect.y;
-    this.context.beginPath();
-    this.context.rect(x, y, w, h);
-    this.context.closePath();
-    this.context.lineWidth = this.settings.rect.lineWidth;
-    this.context.strokeStyle = this.settings.rect.strokeStyle;
-    this.context.stroke();
-    this.rect.w = w;
-    this.rect.h = h;
-}
-
-CropImage.prototype._clear = function() {
+CropImage.prototype._clearCanvas = function() {
     this.context.clearRect(0, 0, this.w, this.h);
 }
 
-
-CropImage.prototype._redraw = function() {
-    this._clear();
-    this._drawImage();
-    this._drawRect();
+CropImage.prototype._deleteCroper = function() {
+    this.body.removeChild(this.croper)
+    this.croper = undefined
 };
 
-CropImage.prototype._start = function() {
-    var self = this;
-    function redraw(){
-        self._redraw()
-    }
-    /*this.drawInterval = setInterval(redraw, 10)*/
-};
 
-CropImage.prototype.stopInterval = function(first_argument) {
-    if(this.drawInterval){
-        clearInterval(this.drawInterval);
-        this.drawInterval = undefined;
-    }
-};
-
-CropImage.prototype._drawImage = function(data, title, is_start) {
-    var data = data || this.img.data
+CropImage.prototype._drawImage = function() {
+    var data = this.img.data
     var imageObj = new Image();
     var self = this;
 
     imageObj.onload = function() {
         var originalW = this.width;
         var originalH = this.height;
-
         self._updateCanvasSize(originalW, originalH);
 
         self.context.drawImage(this, 0, 0);
-        if (is_start){
-            self.rect = {
-                x: (self.w - self.settings.rect.w)/2,
-                y: (self.h - self.settings.rect.h)/2
-            }
-            self.img.data = data
-            self.img.title = title
-        }
-        self._drawRect();
+
+        self._createCroper();
     };
     imageObj.src = data;
 };
 
 CropImage.prototype.draw = function(data, title) {
-    this._drawImage(data, title, true)
+    this._clearCanvas()
+    this.img.data = data;
+    this._drawImage(data, title)
 };
 
 CropImage.prototype.crop = function() {
-    console.log('crop')
     var imageObj = new Image();
     var self = this;
+    var w = this.croper.offsetWidth, h = this.croper.offsetHeight,
+        x = this.croper.offsetLeft, y = this.croper.offsetTop;
 
-    this._clear();
+    this._deleteCroper()
 
     imageObj.onload = function() {
-        self._updateCanvasSize(self.rect.w, self.rect.h);
+        self._updateCanvasSize(w, h);
         self.context.drawImage(this,
-                               self.rect.x, self.rect.y, self.rect.w, self.rect.h, //конфигурация нового куска изображения
-                               0, 0, self.rect.w, self.rect.h //конфигурация позиции этого куска на канвасе
+                               x, y, w, h, //конфигурация нового куска изображения
+                               0, 0, w, h //конфигурация позиции этого куска на канвасе
                                );
         self.base64ImageData = self.canvas.toDataURL();
         console.log(self.base64ImageData)
